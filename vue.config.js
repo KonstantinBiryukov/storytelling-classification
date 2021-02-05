72393d48bdbe483f2e0c70f8ca6668754fa71e0d
Konstantin Biriukov <kostya-leon@mail.ru>
2021-02-05 17:37:52 -0600

    multiple changes in all components, FlyToLocation is added

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-    // publicPath: '/storytelling-classification/',
+    publicPath: '/storytelling-classification/',
     publicPath: process.env.NODE_ENV === "production" ? "/mapbox-vue-example/" : "/",
     indexPath: "/public/index.html",
-    // devServer: {
-    //     proxy: {
-    //         '/api': {
-    //             target: 'http://localhost:8080/class/interactiveMap/video',
-    //             ws: true,
-    //             changeOrigin: true
-    //         }
-    //     }
-    // }
 };
