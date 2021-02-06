41d0baea622d2953ef6201dac4b89045d3604f58
Konstantin Biriukov <kostya-leon@mail.ru>
2021-02-05 19:17:59 -0600

    test

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -6,8 +6,14 @@ const fs = require("fs");
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         console.log("Building...");
         await execa("npm", ["run", "build"]);
+        console.log("npm run build...");
+
         const folderName = fs.existsSync("dist") ? "dist" : "build";
+        console.log("FOlder: " + folderName);
+
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
+        console.log("worktree add all...");
+
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
